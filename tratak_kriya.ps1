81980ebe1945edb2815df6a37ede6f6cbe1975d1
Sharma <sanchit.sharma@gartner.com>
2022-02-24 00:55:04 +0530

    Update tratak_kriya.ps1

--- tratak_kriya.ps1
+++ tratak_kriya.ps1
@@ -4,12 +4,17 @@ $advanced=120 #2min, 4min, 8min = 28 mins
 
 
 $mode=$basic
-Write-Host($mode.ToString() + " seconds is the time constant.")
+Write-Host("`r`nThis is the buffer time of 20 seconds, your practice at the next chime." + "`r`n`r`nTime will increment by " + $mode.ToString() + " seconds each cycle.")
+
+Start-Sleep -Seconds 20 #buffer, to settle down
+
 $OpenWav=New-Object System.Media.SoundPlayer
 $OpenWav.SoundLocation='C:\Windows\Media\Ring06.wav'
 
 $CloseWav=New-Object System.Media.SoundPlayer
 $CloseWav.SoundLocation='C:\Windows\Media\Alarm04.wav'
+
+$OpenWav.playsync() # start the practice
 
 for (($i = 0); $i -lt 3; $i++)
 {
