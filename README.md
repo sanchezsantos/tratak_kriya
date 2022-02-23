e1405d6b357b6caae1ec1f0ef92610b90f4ecbd8
Sharma <sanchit.sharma@gartner.com>
2022-02-23 23:28:49 +0530

    added code and a ReadMe

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # tratak_kriya
+
+This can be used on any windows 10 machine for practice of yogic technique of tratak. It uses default media (.wav) files located at C:\window\media which can be played natively by PS.
+
+How to use:
+
+1. Start the script and the default tone will go off after 30 secs, indicating you to close your eyes.
+2. The next tone will go off after 30 secs, indicating you to open your eyes.
+3. The next cycle is for 1 minute.
+4. The last cycle is for 2 minutes.
+5. You can change the mode to the following or as per requirement.
+    basic=30sec ,1min,2min = 7mins
+    intermediate=1min, 2min, 4min = 14 mins
+    advanced=2min, 4min, 8min = 28 mins
---
+++ tratak_kriya.ps1
@@ -0,0 +1,26 @@
+﻿$basic=30 #30,1min,2min = 7mins
+$intermediate=60 #1min, 2min, 4min = 14 mins
+$advanced=120 #2min, 4min, 8min = 28 mins
+
+
+$mode=$basic
+Write-Host($mode.ToString() + " seconds is the time constant.")
+$OpenWav=New-Object System.Media.SoundPlayer
+$OpenWav.SoundLocation='C:\Windows\Media\Ring06.wav'
+
+$CloseWav=New-Object System.Media.SoundPlayer
+$CloseWav.SoundLocation='C:\Windows\Media\Alarm04.wav'
+
+for (($i = 0); $i -lt 3; $i++)
+{
+    Start-Sleep -Seconds (([Math]::Pow(2,$i)*$mode))
+    Write-Host((([Math]::Pow(2,$i))*$mode).ToString() + " seconds complete. Close your eyes.")
+    $CloseWav.playsync()
+
+    Start-Sleep -Seconds (([Math]::Pow(2,$i))*$mode)
+    Write-Host((([Math]::Pow(2,$i))*$mode).ToString() + " seconds complete. Open your eyes.")
+    $OpenWav.playsync()
+}
+
+
+
